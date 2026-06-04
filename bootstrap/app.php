2fe5c9476d1675607873f16d8889a7a005e13a0b
Victor-Sosa84 <sosacocadavid718@gmail.com>
2026-06-04 07:51:38 -0400

    fix: trustProxies para HTTPS en Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*'); // Confia en el proxy de Railway
         $middleware->alias([
             'privilegio' => \App\Http\Middleware\CheckPrivilegio::class,
         ]);
